9114b539590e28369e1f7b95a2c1ecdec8dccb2a
Eric Dorr <eric.dorr@de-cix.net>
2016-08-30 07:59:22 +0200

    added php script to test php internal memory handling for function calls.

---
+++ test_php_variable_memory_handling.php
@@ -0,0 +1,79 @@
+<?php
+$initital_memory_usage = memory_get_usage(true);
+// register shutfown function to display the final caclulation
+register_shutdown_function('shutdown');
+
+// create test data
+echo "create 100mb.bin testfile" . PHP_EOL;
+system('dd if=/dev/zero of=100mb.bin bs=1024 count=102400 >/dev/null 2>&1');
+
+// instanciate class
+echo "Start of Class. Memory usage: " . (memory_get_usage(true) - $initital_memory_usage) . PHP_EOL;
+$testClass = new TestClass();
+printLine();
+
+// call test function with unset
+echo "Berfore testFunctionWithUnset. Memory usage: " . (memory_get_usage(true) - $initital_memory_usage) . PHP_EOL;
+$testClass->testFunctionWithUnset();
+echo "After testFunctionWithUnset. Memory usage: " . (memory_get_usage(true) - $initital_memory_usage) . PHP_EOL;
+printLine();
+
+// call test class without unset
+echo "Berfore testFunctionWithoutUnset. Memory usage: " . (memory_get_usage(true) - $initital_memory_usage) . PHP_EOL;
+$testClass->testFunctionWithoutUnset();
+echo "After testFunctionWithoutUnset. Memory usage: " . (memory_get_usage(true) - $initital_memory_usage) . PHP_EOL;
+printLine();
+
+// display memory usage after the function calls
+echo "End of Class. Memory usage: " . (memory_get_usage(true) - $initital_memory_usage) . PHP_EOL;
+printLine();
+
+// remove the test data
+echo "delete 100mb.bin testfile" . PHP_EOL;
+unlink('100mb.bin');
+printLine();
+
+/**
+ * The Test Class
+ * @author Eric Dorr
+ */
+class TestClass {
+    public function testFunctionWithoutUnset() {
+        global $initital_memory_usage;
+        echo "Start of testFunctionWithoutUnset. Memory usage: " . (memory_get_usage(true) - $initital_memory_usage) . PHP_EOL;
+        $data = file_get_contents('100mb.bin');
+        echo "End of testFunctionWithoutUnset. Memory usage: " . (memory_get_usage(true) - $initital_memory_usage) . PHP_EOL;
+    }
+
+    public function testFunctionWithUnset() {
+        global $initital_memory_usage;
+        echo "Start of testFunctionWithUnset. Memory usage: " . (memory_get_usage(true) - $initital_memory_usage) . PHP_EOL;
+        $data = file_get_contents('100mb.bin');
+        unset($data);
+        echo "End of testFunctionWithUnset. Memory usage: " . (memory_get_usage(true) - $initital_memory_usage) . PHP_EOL;
+    }
+}
+
+// some helper functions
+function shutdown() {
+    global $initital_memory_usage;
+    echo "End of Script. Memory usage: " . (memory_get_usage(true) - $initital_memory_usage) . PHP_EOL;
+}
+
+/**
+ * @return string
+ */
+function printLine() {
+    echo str_pad("",getScreenWidth(),'#') . PHP_EOL;
+}
+
+/**
+ * @return int the screen width
+ */
+function getScreenWidth() { 
+    preg_match_all("/speed\s([0-9]+)\sbaud;\s([0-9]+)\srows;\s([0-9]+)\scolumns;/", strtolower(exec('stty -a |grep columns')), $output);
+    if(is_array($output)) {
+        return $output[3][0];
+    }
+    return 0;
+}
